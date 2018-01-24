cef9dc815d8de3c73eed62e1ed74f5b0d742193f
vavarachen <vavarachen@users.noreply.github.com>
2018-01-24 15:00:19 -0600

    App name
    Minor fix: app name in the script was incorrect, resulting in failure to get stored credentials.

--- bin/ts_webhook_alert.py
+++ bin/ts_webhook_alert.py
@@ -43,7 +43,7 @@ def get_future_date(days):
 
 def getCredentials(sessionKey):
     """ Retrieve Anomali Threatstream username and API key """
-    myapp = 'ts-webhook-alert'
+    myapp = 'ts_webhook_alert'
     try:
         # list all credentials
         entities = entity.getEntities(['admin', 'passwords'], namespace=myapp,
